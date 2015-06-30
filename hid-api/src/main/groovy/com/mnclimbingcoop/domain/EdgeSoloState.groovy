de321e98e72aa307e6b221d2cc8f08079517f44f
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-06-29 22:44:17 -0500

    adding subproject for api

---
+++ hid-api/src/main/groovy/com/mnclimbingcoop/domain/EdgeSoloState.groovy
@@ -0,0 +1,31 @@
+package com.mnclimbingcoop.domain
+
+import groovy.transform.CompileStatic
+import java.util.concurrent.ConcurrentSkipListSet
+
+@CompileStatic
+class EdgeSoloState {
+
+    String doorName
+
+    Set<AlertAction> alerts = new ConcurrentSkipListSet<AlertAction>()
+
+    Set<CardFormat> cardFormats = new ConcurrentSkipListSet<CardFormat>()
+
+    Set<Cardholder> cardholders = new ConcurrentSkipListSet<Cardholder>()
+
+    Set<Credential> credentials = new ConcurrentSkipListSet<Credential>()
+
+    Set<Door> doors = new ConcurrentSkipListSet<Door>()
+
+    Set<EventMessage> events = new ConcurrentSkipListSet<EventMessage>()
+
+    EdgeSoloParameters parameters
+
+    Set<Reader> readers = new ConcurrentSkipListSet<Reader>()
+
+    Set<Schedule> schedules = new ConcurrentSkipListSet<Schedule>()
+
+    System system
+
+}
