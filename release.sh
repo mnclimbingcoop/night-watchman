b1b60b6628d3393946c8729bb4f6996a3c52562c
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-08-27 08:48:36 -0500

    updating release

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SCOPE=MAJOR
-SCOPE=MINOR
-#SCOPE=PATCH
+#SCOPE=MINOR
+SCOPE=PATCH
 
 gradle -DRELEASE_SCOPE=${SCOPE} -Prelease.stage=final -Prelease.scope=${SCOPE} clean build release bintrayUpload
