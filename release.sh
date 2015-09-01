3a68b75913925560fb18c0ba9195b8b9fd72ff6a
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-09-01 18:16:07 -0500

    making sure tags get pushed to upstream

--- release.sh
+++ release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #SCOPE=MAJOR
-#SCOPE=MINOR
-SCOPE=PATCH
+SCOPE=MINOR
+#SCOPE=PATCH
 
 gradle -DRELEASE_SCOPE=${SCOPE} -Prelease.stage=final -Prelease.scope=${SCOPE} clean build release bintrayUpload
+
+git push upstream --tags
