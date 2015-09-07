097463a3657bb662ed9eacadbbf22f37f7d6a877
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-09-06 19:58:26 -0500

    creating scripts branch

---
+++ start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -1
+
+ARTIFACT=night-watchman
+LOCAL_INSTALL_PATH=`dirname $0`
+LOCAL_FOLDER=${LOCAL_INSTALL_PATH}/.${ARTIFACT}
+JAR_PATH=${LOCAL_FOLDER}/${ARTIFACT}.jar
+
+if [ ! -r ${JAR_PATH} ]; then
+    echo "Unable to find ${JAR_PATH}" 1>&2
+    exit 1
+fi
+
+if [ ! -f ${LOCAL_INSTALL_PATH}/application.properties ]; then
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
+    echo "!! ${LOCAL_INSTALL_PATH}/application.properties not found. !!" 1>&2
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
+    echo "" 1>&2
+    echo "Running application with DEFAULTS. (This will probably not work)" 1>&2
+    echo "" 1>&2
+fi
+
+if [ ! -f ~/.aws/credentials ]; then
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
+    echo "!! ${HOME}/.aws/credentials not found. Please configure. !!" 1>&2
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
+    echo "" 1>&2
+    exit 2
+fi
+
+JAVA_OPTS='-server -Xms512m -Xmx512m'
+echo "Executing: java -jar ${JAR_PATH}"
+pushd ${LOCAL_INSTALL_PATH}
+java ${JAVA_OPTS} -jar ${JAR_PATH}
+popd
+echo $?
+
---
+++ update.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+
+LOCAL_INSTALL_PATH=`dirname $0`
+
+ARTIFACT=night-watchman
+GROUP=com.mnclimbingcoop
+
+DL_URL=https://bintray.com/artifact/download/mnclimbingcoop/maven
+URL=https://bintray.com/mnclimbingcoop/maven/lakitu/_latestVersion
+LATEST_VERSION=`curl -is $URL |grep Location: |sed -e 's#.*https://bintray.com/##' |cut -d \/ -f 4`
+
+if [ "$LATEST_VERSION" == "" ]; then
+    echo "Error fetching latest version!"
+    exit 1
+else
+
+    VERSION=${LATEST_VERSION}
+
+    GROUP_PATH=${GROUP/\./\/}
+    JAR=${ARTIFACT}-${VERSION}.jar
+
+    LOCAL_FOLDER=${LOCAL_INSTALL_PATH}/.${ARTIFACT}
+    JAR_PATH=${LOCAL_FOLDER}/${JAR}
+
+
+    JAR_SYMLINK=${LOCAL_FOLDER}/${ARTIFACT}.jar
+
+
+    if [ ! -d ${LOCAL_FOLDER} ]; then
+        mkdir -p ${LOCAL_FOLDER}
+    fi
+
+    if [ ! -r ${JAR_PATH} ]; then
+        echo "Downloading latest version of ${DL_URL}/${GROUP_PATH}/${ARTIFACT}/${VERSION}/${JAR} to ${JAR_PATH}"
+        curl -L ${DL_URL}/${GROUP_PATH}/${ARTIFACT}/${VERSION}/${JAR} > ${JAR_PATH}
+
+        # If download succeeded
+        if [ -r ${JAR_PATH} ]; then
+            pushd ${LOCAL_FOLDER}
+            ln -sf ${JAR} ${ARTIFACT}.jar
+            popd
+
+        fi
+
+    fi
+fi
